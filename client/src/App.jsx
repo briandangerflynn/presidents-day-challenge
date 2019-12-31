ba231ad7fc4a5c873c9b86bc1e12975d5748417f
David Whitlatch <davidtwhitlatch@gmail.com>
2019-12-31 14:20:41 -0500

    FE: added routes for signup and login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header';
+import Signup from './components/Signup';
+import Login from './components/Login';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
+      <Route path="/login" render={() => (
+        <Login />
+      )} />
+      <Route path="/register" render={() => (
+        <Signup />
+      )} />
       <Route path="/" render={() => (
         <div>Yo Yo Yo!</div>
       )} />
